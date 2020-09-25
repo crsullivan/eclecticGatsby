d9f3900b2717a530e13749a06b3e29ac8e4ccb94
Connor Sullivan <crsullivan@protonmail.com>
2020-09-24 21:35:41 -0700

    swap yarn for npm

--- eclectic/src/components/layout.js
+++ eclectic/src/components/layout.js
@@ -37,7 +37,6 @@ const Layout = () => (
             integrity="sha512-IQLehpLoVS4fNzl7IfH8Iowfm5+RiMGtHykgZJl9AWMgqx0AmJ6cRWcB+GaGVtIsnC4voMfm8f2vwtY+6oPjpQ=="
             crossorigin="anonymous"
             >
-            
       </script>
 
       <Landing />
